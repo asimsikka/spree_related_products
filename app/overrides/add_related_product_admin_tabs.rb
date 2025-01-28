5ccde04249c9396107e8571528230c42a710fa12
Asim Sikka <asim@amroodlabs.com>
2025-01-28 19:16:21 +0500

    fixed the position for related products tab

--- app/overrides/add_related_product_admin_tabs.rb
+++ app/overrides/add_related_product_admin_tabs.rb
@@ -1,14 +1,7 @@
-insert_after_selector =
-  if defined?(Deface::LambdaMatcher)
-    Deface::LambdaMatcher.new do |document|
-      [document.xpath(".//erb").last]
-    end
-  else
-    ':last-child' # TODO: this doesn't work correctly
-  end
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_product_tabs',
   name: 'add_related_products_admin_tab',
-  insert_after: insert_after_selector,
-  partial: 'spree/admin/products/related_products'
+  insert_bottom: '[data-hook="admin_product_tabs"]',
+  partial: 'spree/admin/products/related_products',
+  original: 'c4791d35732b95bae11a32d2f9b6d0e6155f1fea'
 )
